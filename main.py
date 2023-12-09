fc2e3eba6aebe36f1852faaad4b23252bbfd681c
Antuane_Specione <boosla@gmail.com>
2023-12-09 23:20:44 +0300

    fix: change port

--- main.py
+++ main.py
@@ -71,5 +71,5 @@ def generate_image(prompt):
 viber.set_webhook('worker-production-0a9f.up.railway.app/start-viber')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=7590, debug=True)
     
